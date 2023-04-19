c6cfb935841e0e5397d122c852fe78da3cb436db
[SW4/1반] 이수현 <happyshsh72@gmail.com>
2023-04-19 19:25:39 +0900

    Feat: 제품 목록 페이지 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import ProductList from "./pages/user/product-list";
 
-import MainPage from "./pages/user/main-page";
-
-const router = createBrowserRouter([{ path: "/", element: <ProductList /> }]);
+const router = createBrowserRouter([
+  { path: "/product/product_list/:category", element: <ProductList /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
