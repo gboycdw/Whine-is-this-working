27decf09f7fd882fe9de90a354eaaf963f6e64e7
고병욱 <rhquddnr9502@gmail.com>
2023-04-19 15:55:15 +0900

    user 상품상세정보 commit test2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./pages/main-page";
+import ProductDetailPage from "./pages/product-detail-page";
 
-const router = createBrowserRouter([{ path: "/", element: <MainPage /> }]);
+const router = createBrowserRouter([{ path: "/", element: <MainPage /> },{ path: "/ProductDetail", element: <ProductDetailPage /> }]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/product-detail-page.js
@@ -0,0 +1,14 @@
+import Header from "../components/UI/header";
+import Footer from "../components/UI/footer";
+import ProductDetail from "../components/UI/product-detail";
+const ProductDetailPage = () => {
+  return (
+    <>
+      <Header />
+      <ProductDetail></ProductDetail>
+      <Footer />
+    </>
+  );
+};
+
+export default ProductDetailPage;
