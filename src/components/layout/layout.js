501ed68cc71c7488f5f66b0391e7203ea94fbb4c
jang_yoonsu <shagrat8546@gmail.com>
2023-04-21 13:59:54 +0900

    Feat: 메인페이지 기능 구현

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,16 +1,11 @@
 import Footer from "./footer";
 import Header from "./header";
 
-import classes from "./layout.module.css";
-
 const Layout = (props) => {
   return (
     <>
       <Header />
-      <main className={classes.main}>
-        <div class="mb-20"></div>
-        {props.children}
-      </main>
+      <main class="w-[1200px] m-auto">{props.children}</main>
       <Footer />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mod: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      main: "#E5D1D1",
+    },
     extend: {},
   },
   plugins: [],
