6e7ea6fadae72565a05b21da7d90db0130c10e8c
gomao <gboycdw@gmail.com>
2023-04-22 18:02:57 +0900

    [test] backend 변경사항 적용을 위한 commit test

--- src/routers/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./order-router.js";
-export * from "./product-router.js";
-export * from "./user-router.js";
--- src/services/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./order-service.js";
-export * from "./user-service.js";
-export * from './product-services.js';
