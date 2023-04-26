d06d36e50d1c0a2afcc82d362345c89ce5f6b6c4
고병욱 <rhquddnr9502@gmail.com>
2023-04-26 14:49:34 +0900

    Factor: 컴포넌트 간소화

--- FrontEnd/src/components/user/my-page-component/ordered-items-list/ordered-items-list.jsx
+++ FrontEnd/src/components/user/my-page-component/ordered-items-list/ordered-items-list.jsx
@@ -90,51 +90,7 @@ const OrderedItemsList = () => {
       shippingState: "배송완료",
     },
   ];
-  const orderDetailEnter = "주문상세보기>";
-  const orderOptions = ["주문취소", "교환/환불", "리뷰작성", "배송조회"];
-  const OrderedItemsListUp = ({
-    // map 으로 items 뿌리는 함수.
-    orderedItemsId,
-    dateOfOrder,
-    shippingState,
-  }) => {
-    return (
-      <div class="w-[100%]  border-2 border-c3 rounded-xl mb-[20px]">
-        <div class="flex justify-between">
-          <div class="m-[20px] text-xl">{dateOfOrder} 주문</div>
-          <Link to="/" class="m-[20px] text-c4 text-xl">
-            {orderDetailEnter}
-          </Link>
-        </div>
-        <div class="m-[20px] border-2 border-c1 rounded-xl mt-[0px]">
-          <div class="flex justify-between">
-            <div class="text-2xl m-[20px]">{shippingState}</div>
-            {shippingState === "배송준비중" ? (
-              <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
-                {orderOptions[0]}
-              </button>
-            ) : shippingState === "배송완료" ? (
-              <div>
-                <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
-                  {orderOptions[1]}
-                </button>
-                <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
-                  {orderOptions[2]}
-                </button>
-              </div>
-            ) : (
-              <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
-                {orderOptions[3]}
-              </button>
-            )}
-          </div>
-          <div class="mb-[20px]">
-            <OrderedItems />
-          </div>
-        </div>
-      </div>
-    );
-  };
+
   const [dateOfOrder, setDateOfOrder] = useState("");
   return (
     <>
@@ -145,7 +101,7 @@ const OrderedItemsList = () => {
           </div>
           <div>
             {orderedItemsData.slice(offset, offset + limit).map((index) => (
-              <OrderedItemsListUp
+              <OrderedItems
                 dateOfOrder={index.dateOfOrder}
                 shippingState={index.shippingState}
               />
--- FrontEnd/src/components/user/my-page-component/ordered-items-list/ordered-items.jsx
+++ FrontEnd/src/components/user/my-page-component/ordered-items-list/ordered-items.jsx
@@ -1,7 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import OrderedItem from "./ordered-item";
+import { Link } from "react-router-dom";
 
-const OrderedItems = () => {
+const orderDetailEnter = "주문상세보기>";
+const orderOptions = ["주문취소", "교환/환불", "리뷰작성", "배송조회"];
+const OrderedItems = ({
+  // map 으로 items 뿌리는 함수.
+  orderedItemsId,
+  dateOfOrder,
+  shippingState,
+}) => {
   const orderedItem = [
     {
       orderedItemId: "1",
@@ -19,26 +27,50 @@ const OrderedItems = () => {
       orderedItemId: "5",
     },
   ];
-  const ChildItemListUp = ({ orderedItemId }) => {
-    return (
-      <div class="w-[100%] h-[120px]   flex items-center">
-        {/* div's orderedItemId: {orderedItemId} */}
-        <OrderedItem />
-      </div>
-    );
-  };
+
   return (
-    <>
-      <div class=" flex justify-center items-center ">
-        <div class="w-[90%] h-[90%] ">
-          <div>
-            {orderedItem.map((orderedItem) => (
-              <ChildItemListUp orderedItemId={orderedItem.orderedItemId} />
-            ))}
+    <div class="w-[100%]  border-2 border-c3 rounded-xl mb-[20px]">
+      <div class="flex justify-between">
+        <div class="m-[20px] text-xl">{dateOfOrder} 주문</div>
+        <Link to="/" class="m-[20px] text-c4 text-xl">
+          {orderDetailEnter}
+        </Link>
+      </div>
+      <div class="m-[20px] border-2 border-c1 rounded-xl mt-[0px]">
+        <div class="flex justify-between">
+          <div class="text-2xl m-[20px]">{shippingState}</div>
+          {shippingState === "배송준비중" ? (
+            <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
+              {orderOptions[0]}
+            </button>
+          ) : shippingState === "배송완료" ? (
+            <div>
+              <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
+                {orderOptions[1]}
+              </button>
+              <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
+                {orderOptions[2]}
+              </button>
+            </div>
+          ) : (
+            <button class="text-xl border-[1px] border-c1 w-[100px] h-[40px] m-[20px] ">
+              {orderOptions[3]}
+            </button>
+          )}
+        </div>
+        <div class="mb-[20px]">
+          <div class=" flex justify-center items-center ">
+            <div class="w-[90%] h-[90%] ">
+              <div>
+                {orderedItem.map((orderedItem) => (
+                  <OrderedItem orderedItemId={orderedItem.orderedItemId} />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
