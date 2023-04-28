1d19893f066879b880ef69e53085942cba949df5
[SW4/1반] 이수현 <happyshsh72@gmail.com>
2023-04-29 08:31:20 +0900

    Feat: 회원가입 완료 페이지에서 자동 상단 이동

--- FrontEnd/src/pages/auth/signup-complete-page.jsx
+++ FrontEnd/src/pages/auth/signup-complete-page.jsx
@@ -1,9 +1,15 @@
 const SignUpCompletePage = (props) => {
+  // 회원가입 완료 페이지 넘어가자마자 스크롤 위로 올림
+  window.scrollTo({
+    top: 0, // 제일 위로
+    behavior: "smooth", // 부드럽게 이동하는 속성
+  });
+
   return (
     <>
       <div className="flex flex-col items-center">
         {/* 회원가입 완료 Title */}
-        <div className="flex items-center mb-[20px] mt-[50px] mb-[60px]">
+        <div className="flex items-center mb-[20px] mt-[120px] mb-[60px]">
           <h1 className="text-[32px] font-[600]">회원가입 완료</h1>
         </div>
 
