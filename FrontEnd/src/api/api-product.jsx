a8de81a26670e936169221c7cd2279b9771ebe2c
gomao <gboycdw@gmail.com>
2023-04-28 16:15:48 +0900

    frontend folder 삭제 (추후 별도 repo관리)

--- FrontEnd/src/api/api-product.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getAllProduct = async () => {
-  const data = await axios.get(`http://34.22.85.44/api/products`);
-  return data.data;
-};
--- FrontEnd/src/components/user/main/main-article.jsx
+++
@@ -1,5 +0,0 @@
-const MainArticle = () => {
-  return <div class="inline-block h-96"></div>;
-};
-
-export default MainArticle;
